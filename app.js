ea6013182cc1d53a8ca1bae7502411e815fe3273
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-06-18 02:08:02 +0800

    Init restify with Swagger. Environment set up.

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var SwaggerRestify = require('swagger-restify-mw');
+var restify = require('restify');
+var app = restify.createServer();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+
+SwaggerRestify.create(config, function(err, swaggerRestify) {
+  if (err) { throw err; }
+
+  swaggerRestify.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  app.use(
+      function crossOrigin(req,res,next){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        return next();
+      }
+  );
+
+  if (swaggerRestify.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
